7c8df26bfed39fcdb04c86e6d4380b1035f94cbb
Jeswin Kumar <jeswinpk@agilehead.com>
2019-07-08 21:21:34 +0530

    WIP

--- src/api/log.ts
+++ src/api/log.ts
@@ -1,12 +1,22 @@
 import { IRouterContext } from "koa-router";
 import { ensureUserId } from "./authUtils";
-import * as user from "../domain/user";
+import * as log from "../domain/log";
 
-export async function createResource(ctx: IRouterContext) {
-  return ensureUserId(ctx, async userId => {
-    const result = await user.createResource(userId);
-    ctx.body = {
-      success: true
-    };
-  });
+/*
+  We have to:
+    - Define a resource in dreadnought
+    - Treat it as a file? /username/id
+    - Define permissions for the creator
+*/
+export async function createLog(ctx: IRouterContext) {
+  const createLogResult = await log.createLog();
+  return createLogResult.created
+    ? (ctx.body = {
+        id: createLogResult.id
+      })
+    : ((ctx.status = 400), (ctx.body = createLogResult.reason));
+}
+
+export async function appendToLog(ctx: IRouterContext) {
+  const logId = ctx.params.id;
 }
